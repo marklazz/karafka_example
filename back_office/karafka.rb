8d554831c79e6744e4a264d9e18ca3ad04a87f8d
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-04-29 16:32:47 -0300

    Remove unused consumers

--- back_office/karafka.rb
+++ back_office/karafka.rb
@@ -41,14 +41,6 @@ class KarafkaApp < Karafka::App
     # Uncomment this if you use Karafka with ActiveJob
     # You need to define the topic per each queue name you use
     # active_job_topic :default
-    topic :lead do
-      # Uncomment this if you want Karafka to manage your topics configuration
-      # Managing topics configuration via routing will allow you to ensure config consistency
-      # across multiple environments
-      #
-      # config(partitions: 2, 'cleanup.policy': 'compact')
-      consumer ExampleConsumer
-    end
   end
 end
 
