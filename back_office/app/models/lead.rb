67e33c476d6af56a169dfe96090b691b4766143b
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-04-29 16:59:53 -0300

    Refactor to create event as an AR callback

--- back_office/app/models/lead.rb
+++ back_office/app/models/lead.rb
@@ -1,3 +1,15 @@
 class Lead < ApplicationRecord
   belongs_to :user
+  after_create :produce_event_to_karafka
+
+  def produce_event_to_karafka
+    Karafka.producer.produce_sync(topic: 'lead', payload: payload.to_json)
+  end
+
+  def payload
+    {
+      user: user.attributes.slice('email', 'first_name', 'last_name', 'active'),
+      lead: attributes
+    }
+  end
 end
