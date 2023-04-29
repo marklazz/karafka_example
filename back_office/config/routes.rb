b842a33e99c945239cf292c44b9a64c4b8f837a7
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-04-29 15:54:58 -0300

    Add basici auth to karafka status page

--- back_office/config/routes.rb
+++ back_office/config/routes.rb
@@ -2,7 +2,17 @@ require 'karafka/web'
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  mount Karafka::Web::App, at: '/karafka'
+  with_dev_auth = lambda do |app|
+    Rack::Builder.new do
+      use Rack::Auth::Basic do |username, password|
+        username == 'marcelo' && password == 'marcelo'
+      end
+
+      run app
+    end
+  end
+
+  mount with_dev_auth.call(Karafka::Web::App), at: '/karafka'
 
   resources :leads
 
