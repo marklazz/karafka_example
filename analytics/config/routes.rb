a4dd27d167b3c6b5bcda2eb4af85d8697d25c260
Marcelo Giorgi <marklazz.uy@gmail.com>
2023-04-29 15:52:42 -0300

    Add opportunities listing on analytics project

--- analytics/config/routes.rb
+++ analytics/config/routes.rb
@@ -3,4 +3,7 @@ require 'karafka/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount Karafka::Web::App, at: '/karafka'
+  resources :opportunities, only: :index
+
+  root to: 'opportunities#index'
 end
